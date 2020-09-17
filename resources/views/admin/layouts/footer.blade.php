0ba68c681a5b18b8b1a6effffd0916471ca92095
Roger Wu <weayzehoa@gmail.com>
2020-09-17 17:17:56 +0800

    修改視圖

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -1,8 +1,7 @@
 <!-- Main Footer -->
 <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
-    All rights reserved.
+    Copyright &copy; {{ date('Y') }} <strong><a href=" {{ route('index') }} " title="CodingLab Website">CodingLab</a> </strong>All rights reserved. Design by <a href="mailto:weayzehoa@gmail.com" title="Send a mail to Roger."><b>Roger Wu</b></a>.
     <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 3.0.5
+        Base on <strong> <a href="http://adminlte.io" target="_blank" title="AdminLTE Bootstrap Admin Dashboard Template.">AdminLTE.io</a>. </strong><b>Version</b> 3.0.5
     </div>
 </footer>

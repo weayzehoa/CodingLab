0d9ed580c9bb21b072ed64f008468de2ad4b4c02
Roger Wu <weayzehoa@gmail.com>
2020-10-01 11:54:31 +0800

    修正前台套件位置

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,5 +1,5 @@
 <!-- 共用樣板 head.blade.php -->
-<link rel="stylesheet" type="text/css" href="{{ asset('bower/bootstrap/dist/css/bootstrap.min.css') }}" />
-<link rel="stylesheet" type="text/css" href="{{ asset('bower/font-awesome/css/all.min.css') }}" />
-<script type="text/javascript" src="{{ asset('bower/jquery/dist/jquery.min.js') }} "></script>
-<script type="text/javascript" src="{{ asset('bower/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+<link rel="stylesheet" type="text/css" href="{{ asset('vendor/bootstrap/dist/css/bootstrap.min.css') }}" />
+<link rel="stylesheet" type="text/css" href="{{ asset('vendor/font-awesome/css/all.min.css') }}" />
+<script type="text/javascript" src="{{ asset('vendor/jquery/dist/jquery.min.js') }} "></script>
+<script type="text/javascript" src="{{ asset('vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>

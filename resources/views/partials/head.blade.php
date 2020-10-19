0ab92eeb9e09f17118f511a1236b4b5ed192e75b
Roger Wu <weayzehoa@gmail.com>
2020-10-19 09:49:40 +0800

    修改route及移除無用視圖

--- resources/views/partials/head.blade.php
+++
@@ -1,5 +0,0 @@
-<!-- 共用樣板 head.blade.php -->
-<link rel="stylesheet" type="text/css" href="{{ asset('vendor/bootstrap/dist/css/bootstrap.min.css') }}" />
-<link rel="stylesheet" type="text/css" href="{{ asset('vendor/font-awesome/css/all.min.css') }}" />
-<script type="text/javascript" src="{{ asset('vendor/jquery/dist/jquery.min.js') }} "></script>
-<script type="text/javascript" src="{{ asset('vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>

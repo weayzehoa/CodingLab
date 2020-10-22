30bcfd79ba66997faf415e1250d4e92eef776c25
Roger Wu <weayzehoa@gmail.com>
2020-10-22 21:32:33 +0800

    修改後台Dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,48 +4,42 @@
 
 @section('content')
 
-    <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <div class="content-header">
-            <div class="container-fluid">
-                {{-- alert訊息 --}}
-                @include('admin.layouts.alert_message')
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1 class="m-0 text-dark"><b>Dashboard</b></h1>
-                    </div>
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">後台管理系統</a></li>
-                            <li class="breadcrumb-item active"><a href="{{ url('admin/news') }}">Dashboard</a></li>
-                            <li class="breadcrumb-item active"></li>
-                        </ol>
-                    </div>
+<div class="content-wrapper">
+    <div class="content-header">
+        <div class="container-fluid">
+            {{-- alert訊息 --}}
+            @include('admin.layouts.alert_message')
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0 text-dark"><b>Dashboard</b></h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="{{ route('admin.dashboard') }}">後台管理系統</a></li>
+                        <li class="breadcrumb-item active"><a href="{{ url('admin/news') }}">Dashboard</a></li>
+                        <li class="breadcrumb-item active"></li>
+                    </ol>
                 </div>
             </div>
         </div>
-
-    <!-- Main content -->
-    <section class="content">
-
-        <!-- Default box -->
-        <div class="card">
-          <div class="card-header">
-            {{-- <h3 class="card-title">Title</h3> --}}
-            <div class="card-tools">
-              <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
-                <i class="fas fa-minus"></i></button>
-              <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
-                <i class="fas fa-times"></i></button>
-            </div>
-          </div>
-          <div class="card-body">
-              <h2>抱歉，目前只有會員文章完成，其餘皆是空頁面，請見諒。</h2>
-          </div>
-          <div class="card-footer">
-          </div>
-        </div>
-
-      </section>
     </div>
+    <section class="content">
+        <div class="card">
+            <div class="card-header">
+                {{-- <h3 class="card-title">Title</h3> --}}
+                <div class="card-tools">
+                    <button type="button" class="btn btn-tool" data-card-widget="collapse" data-toggle="tooltip" title="Collapse">
+                        <i class="fas fa-minus"></i></button>
+                    <button type="button" class="btn btn-tool" data-card-widget="remove" data-toggle="tooltip" title="Remove">
+                        <i class="fas fa-times"></i></button>
+                </div>
+            </div>
+            <div class="card-body">
+                <h2>抱歉，目前只有會員文章完成，其餘皆是空頁面，請見諒。</h2>
+            </div>
+            <div class="card-footer">
+            </div>
+        </div>
+    </section>
+</div>
 @endsection

54ab8e12dc5041afad896bf16d34840552d3f15a
Roger Wu <weayzehoa@gmail.com>
2020-10-23 21:23:47 +0800

    新增 Google Map 及 Open StreetMap 測試

--- resources/views/googlemap.blade.php
+++ resources/views/googlemap.blade.php
@@ -11,14 +11,18 @@
                 <div class="card card-danger card-outline">
                     <div class="card-body box-profile">
                         <h3 class="profile-username text-center">Google Map</h3>
-                        <i class="fas fa-info text-danger"></i> 說明
                     </div>
                 </div>
                 <div class="card card-primary card-outline">
                     <div class="card-body box-profile">
-                    {{-- 這邊開始放置 內容頁面 --}}
-
-
+                        {{-- 這邊開始放置 內容頁面 --}}
+                        <div class="row">
+                            <div id="map" class="col-6 mb-3"></div>
+                            <div id="map2" class="col-6">
+                                <iframe width="100%" height="100%" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key={{ env('GOOGLEMAP_API_KEY') }}&q=Space+Needle,Seattle+WA" allowfullscreen></iframe>
+                            </div>
+                            <div id="map3" class="col-12"></div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -29,12 +33,60 @@
 
 @section('css')
 {{-- 這邊放置 CSS 用 (在頁面上方) --}}
+<style>
+    #map {
+        height: 300px;
+    }
+    #map2 {
+        height: 300px;
+    }
+    #map3 {
+        height: 300px;
+    }
+</style>
 @endsection
 
 @section('script')
     {{-- 這邊放置 JS套件 程式用 (在頁面上方) --}}
+    <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLEMAP_API_KEY') }}&callback=initMap&libraries=&v=weekly" defer></script>
 @endsection
 
 @section('CustomScript')
     {{-- 這邊放置 Script 程式用 (在頁面下方) --}}
+    <script>
+        let map;
+        let map2;
+        let map3;
+        let map4;
+        function initMap() {
+            const tpzoo = { lat: 24.999735, lng: 121.581389 };
+            const map = new google.maps.Map(document.getElementById("map"), {
+                zoom: 15,
+                center: tpzoo,
+            });
+            const marker = new google.maps.Marker({
+                position: tpzoo,
+                map: map,
+            });
+            const map3 = new google.maps.Map(document.getElementById("map3"), {
+                center: { lat: 25.033845, lng: 121.564530 },
+                zoom: 18,
+                mapTypeId: "satellite",
+                heading: 90,
+                tilt: 45,
+            });
+        }
+        function rotate90() {
+            const heading = map3.getHeading() || 0;
+            map3.setHeading(heading + 90);
+            }
+
+            function autoRotate() {
+            // Determine if we're showing aerial imagery.
+            if (map3.getTilt() !== 0) {
+                window.setInterval(rotate90, 3000);
+            }
+        }
+    </script>
 @endsection
--- resources/views/openstreetmap.blade.php
+++ resources/views/openstreetmap.blade.php
@@ -11,14 +11,11 @@
                 <div class="card card-danger card-outline">
                     <div class="card-body box-profile">
                         <h3 class="profile-username text-center">Open Street Map</h3>
-                        <i class="fas fa-info text-danger"></i> 說明
                     </div>
                 </div>
                 <div class="card card-primary card-outline">
                     <div class="card-body box-profile">
-                    {{-- 這邊開始放置 內容頁面 --}}
-
-
+                        <div id="map"></div>
                     </div>
                 </div>
             </div>
@@ -28,13 +25,52 @@
 @endsection
 
 @section('css')
-{{-- 這邊放置 CSS 用 (在頁面上方) --}}
+    {{-- OpenStreetMap 及 Leaflet CSS --}}
+    <link rel="stylesheet" href="{{ url('vendor/Leaflet/dist/leaflet.css') }}"/>
+    <link rel="stylesheet" href="{{ url('vendor/Leaflet/dist/Control.FullScreen.css') }}"/>
+    <style>
+        #map {
+            height: 600px;
+        }
+    </style>
 @endsection
 
 @section('script')
-    {{-- 這邊放置 JS套件 程式用 (在頁面上方) --}}
+    {{-- OpenStreetMap 及 Leaflet JS --}}
+    <script src="{{ url('vendor/Leaflet/dist/leaflet.js') }}"></script>
+    <script src="{{ url('vendor/Leaflet/dist/Control.FullScreen.js') }}"></script>
+
 @endsection
 
 @section('CustomScript')
     {{-- 這邊放置 Script 程式用 (在頁面下方) --}}
+    <script>
+        $(function() {
+            //宣告OpenStreetMap
+            var map;
+            //載入中心點位置
+            map = L.map('map', {
+                //關閉滾輪放大縮小
+                // scrollWheelZoom:false,
+                fullscreenControl: true,
+                fullscreenControlOptions: {
+                    position: 'bottomright',
+                    title: '全螢幕模式', // change the title of the button, default Full Screen
+                    titleCancel: '離開全螢幕模式', // change the title of the button when fullscreen is on, default Exit Full Screen
+                    // forceSeparateButton: true, // force seperate button to detach from zoom buttons, default false
+                },
+            }).setView([24.999735 , 121.581389], 15);
+            //以該公園為中心點
+            var marker = L.marker([24.999735, 121.581389]);
+            marker.addTo(map).bindPopup('<span style="color:blue">台北市立動物園</span>').openPopup();
+            //Leaflet與OpenStreetMap版權及Zoomin,Zoomout大小
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                maxZoom: 18, //最大Zoom
+                minZoom: 12, //最小Zoom
+            }).addTo(map);
+            // setTimeout(function(){ map.invalidateSize()}, 500);
+            // map.invalidateSize();
+        });
+    </script>
 @endsection

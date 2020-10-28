8347a9b30ce006e5b1a6b9d6baa15d9f0fc49930
Roger Wu <weayzehoa@gmail.com>
2020-10-28 10:32:05 +0800

    修正預設頭像位置

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -7,7 +7,7 @@
     <div class="sidebar">
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="{{ url('img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+                <img src="{{ asset('img/noavatar.png') }}" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
                 <a href="javascript:" class="d-block">{{ $adminuser->name ?? '' }}</a>

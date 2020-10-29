154d0ef7b9ac8b8a89128caccb82870f0c57715c
Roger Wu <weayzehoa@gmail.com>
2020-10-29 08:24:34 +0800

    安裝 ottaviano/faker-gravatar - 隨機 Gravatar 套件

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -76,6 +76,7 @@
                                                 <li>simplesoftwareio/simple-qrcode - QrCode 套件</li>
                                                 <li>laravel/excel - 匯入匯出 試算表 套件</li>
                                                 <li>bmatovu/laravel-xml (v1.0 for Laravel6)- 匯入匯出 XML 套件</li>
+                                                <li>ottaviano/faker-gravatar - 隨機 Gravatar 套件</li>
                                             </ul>
                                         </div>
                                     </div>

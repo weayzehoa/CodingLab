c19340e66414930d0095b39d1a35173123132b3d
Roger Wu <weayzehoa@gmail.com>
2020-11-02 21:38:55 +0800

    新增筆記

---
+++ Laravel筆記/基本練習筆記/Model 與 Migrations 建立方式.php
@@ -0,0 +1,33 @@
+<?php
+/*
+    //建立migration
+    php artisan make:migration create_flights_table
+
+    //建立 Model 並建立 Migration
+    php artisan make:model Flight -m
+
+    //建立 Model 並建立 factory
+    php artisan make:model Flight --factory
+    php artisan make:model Flight -f
+
+    //建立 Model 並建立 seed
+    php artisan make:model Flight --seed
+    php artisan make:model Flight -s
+
+    //建立 Model 並建立 controller
+    php artisan make:model Flight --controller
+    php artisan make:model Flight -c
+
+    //建立 Model 並建立 controller 且使用 resource 參數
+    php artisan make:model Flight --controller --resource
+    php artisan make:model Flight -c --resource
+
+    //建立 Model 並建立 Migration, Factory, Seed, Controller
+    php artisan make:model Flight -mfsc
+
+    //建立 Model 並建立 Migration, Factory, Seed, Controller 且使用 resource 參數
+    php artisan make:model Flight -mfsc --resource
+
+    //建立額外的追加
+    php artisan make:migration addto_carts_table --table=carts
+*/

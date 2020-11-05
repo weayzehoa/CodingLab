d181b95d3b30c2833a2a7521faf8aab47d161dd3
Roger Wu <weayzehoa@gmail.com>
2020-11-05 17:29:01 +0800

    Line機器人hooks

--- app/Http/Controllers/API/LineBotController.php
+++ app/Http/Controllers/API/LineBotController.php
@@ -2,69 +2,46 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use LINE\LINEBot;
+use LINE\LINEBot\Constant\HTTPHeader;
+use LINE\LINEBot\SignatureValidator;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use Exception;
 
 class LineBotController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
     public function hooks(Request $request)
     {
+        $lineAccessToken = env('LINE_CHANNEL_ACCESS_TOKEN'); //前面申請到的Channel acess token(long-lived)
+        $lineChannelSecret = env('LINE_CHANNEL_SECRET');//前面申請到的Channel secret
 
-        return response('hello world', 200);
+        $signature = $request->headers->get(HTTPHeader::LINE_SIGNATURE);
+        if (!SignatureValidator::validateSignature($request->getContent(), $lineChannelSecret, $signature)) {
+           
+            return;
+        }
+
+        $httpClient = new CurlHTTPClient ($lineAccessToken);
+        $lineBot = new LINEBot($httpClient, ['channelSecret' => $lineChannelSecret]);
+
+        try {
+            $events = $lineBot->parseEventRequest($request->getContent(), $signature);
+            foreach ($events as $event) {
+                $replyToken = $event->getReplyToken();
+                $text = $event->getText();// 得到使用者輸入
+                $lineBot->replyText($replyToken, $text);// 回復使用者輸入
+                //$textMessage = new TextMessageBuilder("你好");
+                //$lineBot->replyMessage($replyToken, $textMessage);
+            }
+        } catch (Exception $e) {
+           
+            return;
+        }
+        return;
+        // return response('hello world', 200);
 
         // $params = $request->all();
         // logger(json_encode($params, JSON_UNESCAPED_UNICODE));
